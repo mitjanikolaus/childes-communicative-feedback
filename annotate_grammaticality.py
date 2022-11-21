d04b235ed127405b1cc6986bf879c1026cc4257d
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-11-21 15:09:30 +0100

    Add error category visualizations

--- annotate_grammaticality.py
+++ annotate_grammaticality.py
@@ -4,6 +4,8 @@ import re
 import numpy as np
 import pandas as pd
 import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
@@ -68,12 +70,46 @@ def annotate_grammaticality(clean_utterances, model_name, label_empty_utterance=
     return grammaticalities
 
 
-def annotate(args):
-    utterances = pd.read_csv(args.utterances_file, index_col=0)
+def plot_error_type_stats(utterances):
+    utts = utterances.dropna(subset=["is_grammatical_m", "categories"]).copy()
+    utts["category"] = utts.categories.astype(str).apply(lambda x: x.replace("?", "").split(", "))
+    utts.drop(columns="categories", inplace=True)
+    utts = utts.explode("category")
+    utts.category.value_counts().plot(kind="barh")
+    plt.subplots_adjust(left=0.2, right=0.99)
 
+
+def plot_errors(utterances):
+    plt.figure()
+    utts = utterances.dropna(subset=["is_grammatical_m", "categories"]).copy()
+    utts["category"] = utts.categories.astype(str).apply(lambda x: x.replace("?", "").split(", "))
+    utts.drop(columns="categories", inplace=True)
+    keep_columns = [column for column in utts.columns if "_is_correct" in column or column == "category"]
+    utts = utts[keep_columns]
+    utts = utts.explode("category")
+    utts_grouped = utts.groupby("category").mean()
+    utts_melted = utts_grouped.reset_index().melt("category", var_name='cols', value_name='vals')
+    sns.barplot(data=utts_melted, x="category", y="vals", hue="cols")
+    plt.xticks(rotation=90)
+    plt.subplots_adjust(bottom=0.25, top=0.99)
+    plt.legend(loc='lower left', fontsize='5')
+
+
+def column_name_model_grammaticality(model_name):
+    return "is_grammatical_" + model_name.replace('/', '_')
+
+
+def column_name_model_correct(model_name):
+    return model_name.replace('/', '_') + "_is_correct"
+
+
+def annotate(utterances):
     for model_name in args.grammaticality_annotation_models:
+        column_name = column_name_model_grammaticality(model_name)
+        if column_name in utterances.columns:
+            print(f"Annotation for {model_name} already done. Skipping.")
+            continue
         print(f"Annotating grammaticality with {model_name}..")
-        column_name = "is_grammatical_" + model_name.replace('/', '_')
         utterances[column_name] = annotate_grammaticality(utterances.utt_clean.values, model_name)
 
     if "is_grammatical_m" in utterances.columns:
@@ -83,13 +119,16 @@ def annotate(args):
 
         results = []
         for model_name in args.grammaticality_annotation_models:
-            column_name = "is_grammatical_" + model_name.replace('/', '_')
+            column_name = column_name_model_grammaticality(model_name)
+            column_name_correct = column_name_model_correct(model_name)
+
             utterances[column_name] = utterances[column_name].astype(bool)
-            acc = (utterances[column_name] == utterances.is_grammatical_m).mean()
+            utterances[column_name_correct] = utterances[column_name] == utterances.is_grammatical_m
+            acc = utterances[column_name_correct].mean()
             utt_pos = utterances[utterances.is_grammatical_m]
-            acc_pos = (utt_pos[column_name] == utt_pos.is_grammatical_m).mean()
+            acc_pos = utt_pos[column_name_correct].mean()
             utt_neg = utterances[~utterances.is_grammatical_m]
-            acc_neg = (utt_neg[column_name] == utt_neg.is_grammatical_m).mean()
+            acc_neg = utt_neg[column_name_correct].mean()
 
             results.append({"model": model_name, "accuracy": acc, "accuracy (pos)": acc_pos, "accuracy (neg)": acc_neg})
 
@@ -130,7 +169,13 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    annotated_utts = annotate(args)
+    utterances = pd.read_csv(args.utterances_file, index_col=0)
+    plot_error_type_stats(utterances)
+
+    annotated_utts = annotate(utterances)
+    plot_errors(annotated_utts)
 
     annotated_utts.to_pickle(args.out_file)
     annotated_utts.to_csv(args.out_file.replace(".p", ".csv"))
+
+    plt.show()
