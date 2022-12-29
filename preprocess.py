2dd15108111b083e7ed77dd9a444a450b7657ba9
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-12-29 22:57:38 +0100

    Replace untranscribed names in MPI-EVA-Manchester corpus

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,13 @@
 import argparse
+import itertools
 import os
+import re
 
 import pandas as pd
 import pylangacq
 from tqdm import tqdm
 
 from utils import (
-    remove_superfluous_annotations,
     is_empty,
     POS_PUNCTUATION,
     PREPROCESSED_UTTERANCES_FILE,
@@ -14,10 +15,10 @@ from utils import (
     get_all_paralinguistic_events,
     remove_punctuation,
     get_paralinguistic_event,
-    paralinguistic_event_is_external, clean_utterance,
+    paralinguistic_event_is_external, clean_utterance, remove_timing_information, SPEAKER_CODES_CAREGIVER,
 )
 
-
+NAMES_PATH = "data/names.csv"
 CHILDES_DATA_BASE_PATH = os.path.expanduser(f"~/data/CHILDES/")
 
 
@@ -60,7 +61,71 @@ def get_gra_tag(tag):
         return None
 
 
-def preprocess_utterances(corpus, transcripts, args):
+def replace_untranscribed_names(utterances):
+    """Use common English names to replace untranscribed names"""
+
+    names = pd.read_csv(NAMES_PATH, index_col=0)
+    names = names.sort_values(by="percent", ascending=False)
+
+    words_not_transcribed = []
+    for utt in utterances.transcript_raw.values:
+        words = utt.split(" ")
+        words_not_transcribed.extend(list(itertools.chain(*[re.findall("[_|A-Z|a-z]+www", word) for word in words])))
+
+    name_templates = sorted(set(words_not_transcribed))
+
+    name_dict = dict()
+    for name in name_templates:
+        name_no_www = re.sub("[www[w]+_[W]*www[w]*|www[w]*", "", name)
+        first_letter = name_no_www[-1].upper()
+        if len(name_no_www) == 2:
+            first_letter = name_no_www[0].upper()
+        candidates = names[names.name.str.startswith(first_letter)].name
+
+        prefix = ""
+        candidate_2 = None
+        if len(name_no_www) > 2:
+            prefix = name_no_www
+            if "_" in name:
+                prefix = name.split("_")[0]
+            if first_letter == "_":
+                if prefix in ["Mrs", "Miss", "Aunty", "Auntie", "Missus", "Jeannine", "Gina", "Eleanor",
+                              "Granny"]:
+                    candidates = names[names.sex == "girl"].name
+                if prefix in ["Uncle"]:
+                    candidates = names[names.sex == "boy"].name
+        if prefix[:-1] in ["Mummypig", "Uncle", "Auntie"]:
+            prefix = prefix[:-1]
+        if "www" in prefix:
+            first_letter_2 = re.sub("[www[w]*_[W]*www[w]*|www[w]*", "", prefix)[-1].upper()
+            candidates_2 = names[names.name.str.startswith(first_letter_2)].name
+            candidate_2 = candidates_2.iloc[0]
+        i = 0
+        candidate = (prefix + " " + candidates.iloc[i]).strip()
+        if candidate_2:
+            candidate = candidate_2 + " " + candidates.iloc[i]
+        while candidate in name_dict.values():
+            i += 1
+            candidate = (prefix + " " + candidates.iloc[i]).strip()
+            if candidate_2:
+                candidate = candidate_2 + " " + candidates.iloc[i]
+        name_dict[name] = candidate
+
+    def replace_names(utt):
+        matches = re.findall("[_|A-Z|a-z]+www", utt)
+        for match in matches:
+            if match not in ["eeeowww", "miaoowww"]:
+                new_name = name_dict[match]
+                if "_" in match:
+                    new_name = match.split("_")[0] + " " + new_name
+                utt = utt.replace(match, new_name)
+        return utt
+
+    utterances["transcript_raw"] = utterances.transcript_raw.apply(replace_names)
+    return utterances
+
+
+def preprocess_utterances(corpus, transcripts, start_index, args):
     file_paths = transcripts.file_paths()
 
     ages = transcripts.ages(months=True)
@@ -86,7 +151,6 @@ def preprocess_utterances(corpus, transcripts, args):
     ), total=len(file_paths)):
         # Filter out empty transcripts and transcripts without age or child information
         if len(utts_transcript) == 0:
-            # print("Empty transcript: ", file)
             continue
         if "Interview" in file:
             # Interview transcripts do not contain child-caregiver interactions
@@ -131,8 +195,8 @@ def preprocess_utterances(corpus, transcripts, args):
                         get_gra_tag(t.gra)
                         for t in utt.tokens
                     ],
-                    "start_time": utt.time_marks[0] if utt.time_marks else None,
-                    "end_time": utt.time_marks[1] if utt.time_marks else None,
+                    "start_time": utt.time_marks[0] if utt.time_marks else pd.NA,
+                    "end_time": utt.time_marks[1] if utt.time_marks else pd.NA,
                     "age": round(age),
                     "corpus": corpus,
                     "transcript_file": file_path,
@@ -145,16 +209,19 @@ def preprocess_utterances(corpus, transcripts, args):
         if len(utts_transcript[utts_transcript.speaker_code == SPEAKER_CODE_CHILD]) == 0:
             # Child is not present in transcript, ignore
             continue
+        if len(set(utts_transcript.speaker_code.unique()) & set(SPEAKER_CODES_CAREGIVER)) ==  0:
+            # Caregiver is not present in transcript, ignore
+            continue
 
         if args.require_timing_information:
             # Verify that we have at least timing information for some of the utterances
             if len(utts_transcript["start_time"].dropna()) == 0:
                 continue
 
+        utts_transcript.dropna(subset=["transcript_raw"], inplace=True)
         utts_transcript["transcript_raw"] = utts_transcript["transcript_raw"].apply(
-            remove_superfluous_annotations
+            remove_timing_information
         )
-        utts_transcript.dropna(subset=("transcript_raw",), inplace=True)
 
         utts_transcript = utts_transcript[
             ~utts_transcript.transcript_raw.apply(has_multiple_events)
@@ -164,19 +231,25 @@ def preprocess_utterances(corpus, transcripts, args):
         ]
         utts_transcript = utts_transcript[~utts_transcript.transcript_raw.apply(is_empty)]
 
-        utts_transcript["transcript_clean"] = utts_transcript.transcript_raw.apply(clean_utterance)
-
         utts_transcript = utts_transcript[utts_transcript["transcript_raw"] != ""]
         utts_transcript.dropna(subset=["transcript_raw", "speaker_code"], inplace=True)
-
-        utts_transcript["speaker_code_next"] = utts_transcript.speaker_code.shift(-1)
-        utts_transcript["start_time_next"] = utts_transcript.start_time.shift(-1)
 
         all_utts.append(utts_transcript)
 
     utterances = pd.concat(all_utts, ignore_index=True)
 
-    return utterances
+    if corpus == "MPI-EVA-Manchester":
+        utterances = replace_untranscribed_names(utterances)
+
+    utterances["transcript_clean"] = utterances.transcript_raw.apply(clean_utterance)
+
+    utterances.index = pd.RangeIndex(start_index, start_index+len(utterances))
+    if start_index == 0:
+        utterances.to_csv(args.out)
+    else:
+        utterances.to_csv(args.out, mode="a", header=False)
+
+    return start_index+len(utterances)
 
 
 def has_multiple_events(utterance):
@@ -195,6 +268,7 @@ def is_external_event(utterance):
 
 def preprocess_transcripts(args):
     all_utterances = []
+    start_index = 0
     for corpus in args.corpora:
         print(f"Reading transcripts of {corpus} corpus.. ", end="")
         transcripts = pylangacq.read_chat(
@@ -203,11 +277,7 @@ def preprocess_transcripts(args):
         print("done.")
 
         print(f"Preprocessing utterances of {corpus}.. ")
-        utterances_corpus = preprocess_utterances(corpus, transcripts, args)
-
-        all_utterances.append(utterances_corpus)
-
-    all_utterances = pd.concat(all_utterances, ignore_index=True)
+        start_index = preprocess_utterances(corpus, transcripts, start_index, args)
 
     return all_utterances
 
@@ -218,7 +288,5 @@ if __name__ == "__main__":
     if not args.out.endswith(".csv"):
         raise ValueError("Out file should have .csv ending!")
 
-    preprocessed_utterances = preprocess_transcripts(args)
-
     os.makedirs(os.path.dirname(args.out), exist_ok=True)
-    preprocessed_utterances.to_csv(args.out)
+    preprocess_transcripts(args)
