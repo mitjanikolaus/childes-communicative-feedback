06df1441ee1c46645c1af984bb0baedcb24cd712
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-01-08 11:37:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='communicative_feedback',
+    version='',
+    packages=['grammaticality_annotation'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
