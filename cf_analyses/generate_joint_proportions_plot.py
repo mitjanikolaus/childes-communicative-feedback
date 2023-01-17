e608e40a327659421f7b1f2cfcd77a64072c301c
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-01-17 10:30:11 +0100

    update joint proportions plot script

--- cf_analyses/generate_joint_proportions_plot.py
+++ cf_analyses/generate_joint_proportions_plot.py
@@ -5,6 +5,12 @@ import seaborn as sns
 import os
 
 from cf_analyses.analysis_reproduce_warlaumont import AGE_BIN_NUM_MONTHS
+from utils import MICRO_CONVERSATIONS_FILE, filter_transcripts_based_on_num_child_utts
+
+MIN_AGE = 10
+MAX_AGE = 60
+
+DEFAULT_MIN_CHILD_UTTS_PER_TRANSCRIPT = 10
 
 
 def make_proportion_plots(conversations, results_dir):
@@ -21,6 +27,7 @@ def make_proportion_plots(conversations, results_dir):
         logistic=True,
         line_kws={"color": sns.color_palette("tab10")[0]},
         scatter_kws={"alpha": 0.2, "s": 20, "color": sns.color_palette("tab10")[0]},
+        label="proportion_speech_like",
     )
 
     proportion_intelligible_per_transcript = conversations.groupby(
@@ -34,12 +41,26 @@ def make_proportion_plots(conversations, results_dir):
         marker=".",
         line_kws={"color": sns.color_palette("tab10")[1]},
         scatter_kws={"alpha": 0.2, "s": 20, "color": sns.color_palette("tab10")[1]},
+        label="proportion_intelligible",
+    )
+
+    proportion_grammatical_per_transcript = conversations.groupby(
+        "transcript_file"
+    ).agg({"utt_is_grammatical": "mean", "age": "mean"})
+    sns.regplot(
+        data=proportion_grammatical_per_transcript,
+        x="age",
+        y="utt_is_grammatical",
+        logistic=True,
+        marker=".",
+        line_kws={"color": sns.color_palette("tab10")[2]},
+        scatter_kws={"alpha": 0.2, "s": 20, "color": sns.color_palette("tab10")[2]},
+        label="proportion_grammatical",
     )
     axis.set(xlabel="age (months)", ylabel="")
-    axis.legend(
-        labels=["proportion_speech_like", "proportion_intelligible"],
-        loc="lower right",
-    )
+
+    axis.legend(loc="lower right")
+
     axis.set_xticks(np.arange(12, conversations.age.max() + 1, step=AGE_BIN_NUM_MONTHS))
     plt.tight_layout()
     plt.savefig(os.path.join(results_dir, "proportions.png"), dpi=300)
@@ -47,7 +68,15 @@ def make_proportion_plots(conversations, results_dir):
 
 
 if __name__ == "__main__":
-    conversations = pd.read_csv("results/conversations_raw.csv")
+    # TODO: run with data from all corpora
+    conversations = pd.read_csv(MICRO_CONVERSATIONS_FILE)
+
+    conversations = conversations[
+        (MIN_AGE <= conversations.age) & (conversations.age <= MAX_AGE)
+    ]
+    conversations = filter_transcripts_based_on_num_child_utts(
+        conversations, DEFAULT_MIN_CHILD_UTTS_PER_TRANSCRIPT
+    )
 
     results_dir = "results/"
     make_proportion_plots(conversations, results_dir)
