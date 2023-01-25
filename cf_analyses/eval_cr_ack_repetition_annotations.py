36e241f775f8841df34da6b9f84c99ae6a345233
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-01-25 10:52:05 +0100

    Add CR and ACK manual annotation files

--- cf_analyses/eval_cr_ack_repetition_annotations.py
+++ cf_analyses/eval_cr_ack_repetition_annotations.py
@@ -13,16 +13,16 @@ from utils import PROJECT_ROOT_DIR
 RESULTS_DIR = PROJECT_ROOT_DIR+"/results/cr_ack/"
 
 
-def eval_crs():
-    annotated = pd.read_csv(PROJECT_ROOT_DIR+"/data/CR_manual_annotations.csv", index_col=0)
+def eval_crs(annotations_file):
+    annotated = pd.read_csv(annotations_file, index_col=0)
 
     repetition_ratios = annotated.apply(get_repetition_ratios, axis=1)
     annotated["rep_utt"] = repetition_ratios.apply(lambda ratios: ratios[0])
     annotated["rep_response"] = repetition_ratios.apply(lambda ratios: ratios[1])
 
     plt.figure(figsize=(5, 4))
-    counts = annotated.groupby(['rep_utt', 'rep_response', "is_clarification_request"]).size().reset_index(name='number')
-    ax = sns.scatterplot(data=counts, x="rep_utt", y="rep_response", hue="is_clarification_request", size="number", sizes=(30, 1000), alpha=0.8)
+    counts = annotated.groupby(['rep_utt', 'rep_response', "is_cr"]).size().reset_index(name='number')
+    ax = sns.scatterplot(data=counts, x="rep_utt", y="rep_response", hue="is_cr", size="number", sizes=(30, 1000), alpha=0.8)
     handles, labels = ax.get_legend_handles_labels()
     handles = handles[3:]
     labels = labels[3:]
@@ -35,14 +35,14 @@ def eval_crs():
     plt.savefig(os.path.join(RESULTS_DIR, "repetition_cr.png"), dpi=300)
 
     annotated["is_cr_auto_rep"] = annotated.apply(is_repetition_clarification_request, axis=1).astype(int)
-    prf = precision_recall_fscore_support(annotated["is_clarification_request"], annotated["is_cr_auto_rep"], average="binary")
-    print("Precision, recall, f-score: ", prf)
-    print("\n Misclassified: ")
-    print(annotated[annotated.is_clarification_request != annotated.is_cr_auto_rep].to_string(index=False))
+    prf = precision_recall_fscore_support(annotated["is_cr"], annotated["is_cr_auto_rep"], average="binary")
+    print("\n Precision, recall, f-score: ", prf)
+    print("Misclassified: ")
+    print(annotated[annotated.is_cr != annotated.is_cr_auto_rep].to_string(index=False))
 
 
-def eval_acks():
-    annotated = pd.read_csv(PROJECT_ROOT_DIR+"/data/ACK_manual_annotations.csv", index_col=0)
+def eval_acks(annoations_file):
+    annotated = pd.read_csv(annoations_file, index_col=0)
 
     repetition_ratios = annotated.apply(get_repetition_ratios, axis=1)
     annotated["rep_utt"] = repetition_ratios.apply(lambda ratios: ratios[0])
@@ -66,23 +66,28 @@ def eval_acks():
 
     annotated["is_ack_auto_rep"] = annotated.apply(response_is_acknowledgement, axis=1).astype(int)
     prf = precision_recall_fscore_support(annotated["is_ack"], annotated["is_ack_auto_rep"], average="binary")
-    print("Precision, recall, f-score: ", prf)
+    print("\n Precision, recall, f-score: ", prf)
 
-    print("\n Misclassified: ")
+    print("Misclassified: ")
     print(annotated[annotated.is_ack != annotated.is_ack_auto_rep].to_string(index=False))
 
 
+def generate_data_for_annotation(for_clarification_requests: bool):
+    conversations = pd.read_csv("results/grammaticality/conversations.csv", index_col=0)
+    convs = conversations[
+        (conversations.rep_utt > 0.0) & (conversations.rep_response > 0.0) & (
+                conversations.response_transcript_clean.str.endswith("?") == for_clarification_requests)].sample(100, random_state=1)[
+        ["utt_transcript_clean", "response_transcript_clean"]]
+    convs.to_csv("CR_manual_annotations.csv")
+
+
 if __name__ == "__main__":
-    # conversations = pd.read_csv("results/grammaticality/conversations.csv", index_col=0)
-
-    # convs = conversations[
-    #     (conversations.utt_repetition_ratio > 0.0) & (conversations.resp_repetition_ratio > 0.0) & (
-    #             conversations.response_transcript_clean.str.endswith("?") == False)].sample(100, random_state=1)[
-    #     ["utt_transcript_clean", "response_transcript_clean", "utt_repetition_ratio", "resp_repetition_ratio"]]
-    # convs.to_csv("ACK_manual_annotations.csv")
-
     os.makedirs(RESULTS_DIR, exist_ok=True)
-    eval_crs()
-    eval_acks()
+
+    eval_crs(PROJECT_ROOT_DIR+"/data/CR_manual_annotations.csv")
+    eval_acks(PROJECT_ROOT_DIR+"/data/ACK_manual_annotations.csv")
+
+    eval_crs(PROJECT_ROOT_DIR+"/data/CR_manual_annotations_test.csv")
+    eval_acks(PROJECT_ROOT_DIR+"/data/ACK_manual_annotations_test.csv")
 
     plt.show()
