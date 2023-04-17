254acbd65a4c8037cac9b51dc3f09dcfd877fd75
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2023-04-17 18:52:53 +0200

    Update eval_manual_annotation.py

--- grammaticality_manual_annotation/eval_manual_annotation.py
+++ grammaticality_manual_annotation/eval_manual_annotation.py
@@ -1,58 +1,78 @@
 import itertools
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+
 import pandas as pd
 from krippendorff import krippendorff
 from sklearn.metrics import cohen_kappa_score, matthews_corrcoef
 
 from utils import PROJECT_ROOT_DIR
 
+BASE_PATH = PROJECT_ROOT_DIR + "/data/manual_annotation/selection/"
+ANNOTATORS = ["mit", "ale"]
+
 
 def eval():
-    base_path = PROJECT_ROOT_DIR+"/data/manual_annotation"
-    data = pd.read_csv(os.path.join(base_path, "grammaticality_manual_annotation_400_600_mit.csv"), index_col=0)
+    files = [f for f in os.listdir(BASE_PATH) if os.path.isfile(os.path.join(BASE_PATH, f))]
 
-    columns = ["is_grammatical"]
-    for suffix in ["abd", "abh", "dhi", "lau"]: # abd", "abh", "dhi",
-        ann_file = os.path.join(base_path, f"grammaticality_manual_annotation_400_600_{suffix}.csv")
-        utts = pd.read_csv(ann_file, index_col=0)
-        column_name = f"is_grammatical_{suffix}"
-        data[column_name] = utts["is_grammatical"]
+    files_ann = {}
+    for ann in ANNOTATORS:
+        files_ann[ann] = sorted([f for f in files if f[-7:-4] == ann])
+
+    base_ann = ANNOTATORS[1]
+    data = []
+    for file in files_ann[base_ann]:
+        data.append(pd.read_csv(os.path.join(BASE_PATH, file), index_col=0))
+
+    data = pd.concat(data, ignore_index=True)
+
+    columns = ["has_error"]
+    for ann in ANNOTATORS:
+        if ann == base_ann:
+            continue
+
+        data_ann = []
+        for file in files_ann[ann]:
+            data_ann.append(pd.read_csv(os.path.join(BASE_PATH, file), index_col=0))
+        data_ann = pd.concat(data_ann, ignore_index=True)
+
+        assert len(data_ann.dropna(subset=["has_error"])) == len(data.dropna(subset=["has_error"]))
+        column_name = f"has_error_{ann}"
+        data[column_name] = data_ann["has_error"]
         columns.append(column_name)
 
-        # data[f"labels_{suffix}"] = utts["labels"]
-        # data[f"note_{suffix}"] = utts["note"]
+    data["disagreement"] = (~data.has_error.isna() & (data.has_error != data.has_error_mit))
+    data["disagreement"] = data.disagreement.replace({True: 1, False: ""})
+    data.to_csv(os.path.join(BASE_PATH, "agreement.csv"))
 
-
-    all_agree = data[(data.is_grammatical == data.is_grammatical_abh) & (data.is_grammatical == data.is_grammatical_abd) & (data.is_grammatical == data.is_grammatical_lau) & (data.is_grammatical == data.is_grammatical_dhi)]
-    somebody_disagrees = data[~data.index.isin(all_agree.index)]
-    print("len (all_agree): ", len(all_agree))
-
-    # disagree_strong = data[(data.is_grammatical == 1) & (data.is_grammatical_lau == -1) |
-    #                 (data.is_grammatical == -1) & (data.is_grammatical_lau == 1)]
-    # #
-    # disagree = data[(data.is_grammatical != data.is_grammatical_lau)]
+    data.dropna(subset=["has_error"], inplace=True)
 
     kappa_scores = []
     mcc_scores = []
     for ann_1, ann_2 in itertools.combinations(columns, 2):
         kappa = cohen_kappa_score(data[ann_1], data[ann_2])
         kappa_scores.append(kappa)
-        print(f"Kappa {(ann_1, ann_2)}: {kappa}")
+        print(f"Kappa {(ann_1, ann_2)}: {kappa:.2f}")
 
         mcc = matthews_corrcoef(data[ann_1], data[ann_2])
         mcc_scores.append(mcc)
-    print("Kappa:", np.mean(kappa_scores))
+    print(f"Kappa: {np.mean(kappa_scores):.2f}")
 
-    print("MCC:")
-    print(mcc_scores)
-    print(np.mean(mcc_scores))
+    print(f"MCC: {np.mean(mcc_scores):.2f}")
 
-    print("Alpha:")
     rel_data = [data[name] for name in columns]
     alpha = krippendorff.alpha(reliability_data=rel_data)
-    print(alpha)
+    print(f"Alpha: {alpha:.2f}")
+
+
+def eval_disagreement():
+    data = pd.read_csv(os.path.join(BASE_PATH, "disagreement_annotated.csv"))
+    data[data.disagreement == 1].groupby("disagreement_reason").size().sort_values(ascending=False).plot(kind="barh")
+    plt.subplots_adjust(left=0.5)
+    plt.show()
 
 
 if __name__ == "__main__":
     eval()
+    # eval_disagreement()
